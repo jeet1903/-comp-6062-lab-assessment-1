c9190995a65b25ceba53dcb6b32606b15f731835
jeet1903 <jeet1903@gmail.com>
2024-06-11 19:43:53 -0400

    Update app.js

--- LA1/scripts/app.js
+++ LA1/scripts/app.js
@@ -1,8 +1,9 @@
-const studentName = "Jeet Shah"; 
-const studentNumber = "1147343"; 
+//const studentName = "Jeet Shah"; 
+//const studentNumber = 1147343; 
+const result = 'Jeet Shah - 1147343';
 
-console.log(${studentName} - ${studentNumber});
+console.log(result);
 
 const headerContent = document.querySelector('h1');
-headerContent.innerHTML = ${studentName} - ${studentNumber};
+headerContent.innerHTML = result;
 headerContent.classList.add('heading1');
