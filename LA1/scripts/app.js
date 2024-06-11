27a44674da0fd87dc2312ffcdb1c0dacd4d3c2ca
jeet1903 <jeet1903@gmail.com>
2024-06-11 19:30:24 -0400

    Lab Assessment 1
    Web page

---
+++ LA1/scripts/app.js
@@ -0,0 +1,8 @@
+const studentName = "Jeet Shah"; 
+const studentNumber = "1147343"; 
+
+console.log(${studentName} - ${studentNumber});
+
+const headerContent = document.querySelector('h1');
+headerContent.innerHTML = ${studentName} - ${studentNumber};
+headerContent.classList.add('heading1');
